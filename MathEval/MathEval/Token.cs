ccba05fc8ea536ad7bde83999698ca61a50b94f8
matespinda01 <65965744+matespinda01@users.noreply.github.com>
2021-02-09 08:36:18 +0100

    Add token types

---
+++ MathEval/MathEval/Token.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathEval
+{
+    public enum Token
+    {
+        EOF,
+        ADD,
+        SUBSTRACT,
+        DIVIDE,
+        MULT,
+        EXP,
+        NR,
+    }
+}
